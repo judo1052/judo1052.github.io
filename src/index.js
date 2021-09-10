f43c76b0a8293f01d7b7c98a4b10bae5efb3bbec
judo1052 <judo00119@gmail.com>
2021-09-10 14:03:24 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+
+// // function FlexColumnDiv() {
+// //   return (
+// //     <div className="flex flex-row justify-center">
+// //       <div className="gap-4 py-10 px-5 border-2 border-black">First</div>
+// //       <div className="m-5 p-5 border-2 border-black">Second</div>
+// //       <div className="m-5 p-5 border-2 border-black">Third</div>
+// //       <div className="m-5 p-5 border-2 border-black">Fourth</div>
+// //       <div className="m-5 p-5 border-2 border-black">Five</div>
+// //     </div>
+// //   );
+// // }
 
 function FlexColumnDiv2() {
   return (
